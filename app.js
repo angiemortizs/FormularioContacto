4330920c42a58b8f455d70aa55b0e91cc266bb0f
angiemortizs <angiemortizs@gmail.com>
2024-08-03 12:14:08 -0500

    A

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const frmEmail = document.getElementById('frm-email')
-frmEmail.addEventListener('sumbit', sendEmail)
+const frmEmail = document.getElementById("frm-email")
+frmEmail.addEventListener("submit", sendEmail)
 
-const serviceId = 'service_l17j9vt'
-const templateId = 'template_21zzhny'
-const apikey = 'Vg_XPUT32tEEtfBFo'
+const serviceId = "service_u7lwr33"
+const templateId = "template_gt0vmty"
+const apikey = "Vg_XPUT32tEEtfBFo" 
 
 function sendEmail(event){
-    event.preventDefault();
-    emailjs.init(serviceId);
-    
+    event.preventDefault()
+    emailjs.init(serviceId)
+
     emailjs.sendForm(serviceId,templateId,frmEmail,apikey)
-    .then((result) => Swal.fire("Su mensaje se ha enviado con éxito"))
-    .catch((error)  => {
+    .then((result) => Swal.fire ("Su mensaje se ha enviado con exito"))
+    .catch((error) => {
         Swal.fire({
             icon: "error",
-            title:"Oops...",
-            text: "No ha sido posible enviar el mensaje!",
-        });
-    });
+            title: "Oops...",
+            text: "No ha sido posible enviar el mensaje",
+    })
+  })
 }
+
